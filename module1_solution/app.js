f0ecb34866dbe577b9500aac664fad0f3b59abfd
mnavidad <mchristmas@yahoo.com>
2017-08-05 23:31:19 -0400

    added color style

--- module1_solution/app.js
+++ module1_solution/app.js
@@ -2,9 +2,53 @@
 'use strict';
 
 angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
 
-.controller('LunchCheckController', function () {
+LunchCheckController.$inject = ['$scope', '$filter'];
+function LunchCheckController($scope, $filter) {
+  $scope.name = "";
+  $scope.msg ="";
+  $scope.customStyle = {};
+  var currentMsg;
 
-});
+  $scope.separateFood = function () {
+
+    $scope.name = splitString($scope.name,",");
+    currentMsg = $scope.name;
+
+    $scope.msg = currentMsg;
+    if($scope.msg=== "Enjoy!" || $scope.msg=== "Too much!"  ){
+      $scope.customStyle.style = {"color":"green"};
+    }
+    else {
+      $scope.customStyle.style = {"color":"red"};
+    }
+  };
+
+  function splitString(stringToSplit, separator) {
+        var arrayOfStrings = stringToSplit.split(separator);
+        var message;
+         console.log('The original string is: "' + stringToSplit + '"');
+         console.log('The separator is: "' + separator + '"');
+         console.log('The array has ' + arrayOfStrings.length + ' elements: ' + arrayOfStrings.join(' / '));
+         if(stringToSplit===""){
+             message= "Please enter data first"
+             return message;
+          }
+
+         if(arrayOfStrings.length<=3){
+           message = "Enjoy!";
+           return message;
+
+          }
+
+          if(arrayOfStrings.length>3){
+              message= "Too much!"
+              return message;
+
+           }
+
+       }
+}
 
 })();
